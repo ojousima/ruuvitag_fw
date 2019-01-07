c8bea71157642e0e9605fa24eeacf3db89fac4ed
Otso Jousimaa <otso@ojousima.net>
2019-01-07 12:24:20 +0200

    Fix advertisement type at startup, bump version

--- ruuvi_examples/ruuvi_firmware/bluetooth_application_config.h
+++ ruuvi_examples/ruuvi_firmware/bluetooth_application_config.h
@@ -7,7 +7,7 @@
 #define APP_DEVICE_NAME_LENGTH          APPLICATION_DEVICE_NAME_LENGTH
 #define APPLICATION_ADV_INTERVAL        1010                            /**< ms. Use value which is not exactly divisible by 1000 ms to be seen by gateways which have limited scan windows in second divisible intervals. **/
 #define APP_TX_POWER                    4                               /**< dBm **/
-#define INIT_FWREV                      "AES 2.4.0"                         /**< Github tag. Do not include specifiers such as "alpha" so you can accept ready binaries as they are **/
+#define INIT_FWREV                      "AES 2.4.1"                         /**< Github tag. Do not include specifiers such as "alpha" so you can accept ready binaries as they are **/
 #define INIT_SWREV                      INIT_FWREV                      /**< FW and SW are same thing in this context **/                             
 
 // milliseconds until main loop timer function is called. Other timers can bring
@@ -37,9 +37,9 @@
 
 // After boot or user interaction, such as button press and NFC read
 #if APP_GATT_PROFILE_ENABLED
-  #define STARTUP_ADVERTISEMENT_TYPE     0x03
-#else
   #define STARTUP_ADVERTISEMENT_TYPE     0x00
+#else
+  #define STARTUP_ADVERTISEMENT_TYPE     0x03
 #endif
 
 #endif
